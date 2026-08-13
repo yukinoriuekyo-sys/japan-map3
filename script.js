61dd237b091663639f59aa6a77bc633c6c5db37c
yukinoriuekyo-sys <yukinoriuekyo@gmail.com>
2026-08-13 12:26:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,662 @@
+const locations = [
+
+     {
+        name: "道北",
+        left: 72,
+        top: 4,
+
+        links: [
+              {
+            name: "樺岡",
+
+            sublinks: [
+                {
+                     name: "windy",
+                url: "https://www.windy.com/45.338/141.820?44.891,141.820,8,i:pressure"
+            },
+            {
+                name: "Yhoo!",
+                url: "https://weather.yahoo.co.jp/weather/jp/1a/1100.html"
+            },
+            {
+                name: "天気.com",
+                url: "https://tenki.jp/forecast/1/1/1100/1214/"
+                },
+            ]
+        },
+         {
+            name: "勇知",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+        {
+            name: "予備",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+    ]
+},
+    
+    {
+        name: "浜頓別",
+        left: 80,
+        top: 10,
+
+        links: [
+           {
+                    name: "windy",
+                    url: "https://www.windy.com/45.111/142.416?44.626,142.416,8,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: "https://weather.yahoo.co.jp/weather/jp/1a/1100/1512.html"
+                },
+                {
+                    name: "天気.com",
+                    url: "https://tenki.jp/forecast/1/1/1100/1512/"
+                }
+        ]
+    },
+
+ {
+        name: "苫前",
+        left: 70,
+        top: 10,
+
+        links: [
+           {
+                    name: "windy",
+                    url: "https://www.windy.com/44.252/141.661?44.249,141.678,15"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                }
+        ]
+    },
+
+    {
+        name: "道南",
+        left: 65,
+        top: 27,
+
+        links: [
+           {
+            name: "江差",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/41.947/140.159?41.482,140.158,8,m:eDcaj1i"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "伊達",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/42.424/140.976?41.917,140.976,8,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+    ]
+},
+
+{
+        name: "青森",
+        left: 70,
+        top: 32,
+
+        links: [
+              {
+            name: "野辺地",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/40.956/141.242?2025052703,40.839,141.241,10"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "北野沢",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+        {
+            name: "予備",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+    ]
+},
+
+    {
+        name: "秋田",
+        left: 65,
+        top: 40,
+
+        links: [
+              {
+            name: "秋田港",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "由利本荘",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+        {
+            name: "東由利原",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: ""
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+    ]
+},
+
+{
+        name: "石巻",
+        left: 72,
+        top: 48,
+
+        links: [
+           {
+                name: "windy",
+                url: "https://www.windy.com/38.479/141.339?38.508,141.776,10"
+            },
+            {
+                name: "Yhoo!",
+                url: "/aomori/hotel"
+            },
+            {
+                name: "天気.com",
+                url: "/aomori/access"
+            }
+        ]
+    },
+
+ {
+    name: "予備",
+    left: 40,
+    top: 54,
+
+    links: [
+        {
+            name: "予備",
+            url: ""
+        },
+
+        {
+            name: "予備",
+            url: ""
+        },
+
+        {
+            name: "予備",
+
+            sublinks: [
+                {
+                    name: "予備",
+                    url: ""
+                },
+                {
+                    name: "予備",
+                    url: ""
+                },
+                {
+                    name: "予備",
+                    url: ""
+                },
+                {
+                    name: "予備",
+                    url: ""
+                }
+            ]
+        }
+    ]
+},
+
+    {
+        name: "福島",
+        left: 70,
+        top: 56,
+
+        links: [
+             {
+            name: "滝根",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/37.331/140.719?36.835,140.719,8"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "阿武隈",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/37.410/140.856?37.217,140.856,9,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: "https://weather.yahoo.co.jp/weather/7/3620/7545.html"
+                },
+                {
+                    name: "天気.com",
+                    url: "https://tenki.jp/forecast/2/10/3620/7545/"
+                },
+            ]
+        },
+        {
+            name: "飯舘",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/37.672/140.670?37.129,140.670,8,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+        ]
+    },
+
+    {
+        name: "河津",
+        left: 60,
+        top: 72,
+
+           links: [
+            {
+                name: "windy",
+                url: "https://urldefense.com/v3/__https://www.windy.com/34.801/138.993?34.737,138.993,11,m:eDcaj1j__;!!IIc8w16ASern!oyJZs2946vVJ-f9fmxqKsYZAp1KCjv4YVZUJTTmhcfTpR7jvc4km7cnpq58Zx7pH03cbNIAqi2dJ7sf3PMmc47jciidM32hQbw$"
+            },
+            {
+                name: "Yhoo!",
+                url: ""
+            },
+            {
+                name: "天気.com",
+                url: ""
+            }
+        ]
+    },
+
+    {
+        name: "新出雲",
+        left: 25,
+        top: 68,
+
+        links: [
+           {
+                    name: "windy",
+                    url: "https://www.windy.com/35.472/132.764?34.855,132.764,8"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                }
+        ]
+    },
+
+{
+        name: "和歌山",
+        left: 40,
+        top: 76,
+
+        links: [
+             {
+            name: "有田川",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/34.098/135.190?33.551,136.956,7,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "大川原",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://www.windy.com/33.953/134.408?33.338,134.408,8,i:pressure"
+                },
+                {
+                    name: "Yhoo!",
+                    url: "https://weather.yahoo.co.jp/weather/36/7110/36321.html"
+                },
+                {
+                    name: "天気.com",
+                    url: "https://tenki.jp/forecast/8/39/7110/36321/"
+                },
+            ]
+        }
+        ]
+    },
+
+    {
+        name: "河津",
+        left: 60,
+        top: 72,
+
+           links: [
+            {
+                name: "windy",
+                url: "https://urldefense.com/v3/__https://www.windy.com/34.801/138.993?34.737,138.993,11,m:eDcaj1j__;!!IIc8w16ASern!oyJZs2946vVJ-f9fmxqKsYZAp1KCjv4YVZUJTTmhcfTpR7jvc4km7cnpq58Zx7pH03cbNIAqi2dJ7sf3PMmc47jciidM32hQbw$"
+            },
+            {
+                name: "Yhoo!",
+                url: ""
+            },
+            {
+                name: "天気.com",
+                url: ""
+            }
+        ]
+    },
+
+
+    {
+        name: "鹿児島",
+        left: 16,
+        top: 93,
+
+        links: [
+           {
+            name: "輝北",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://urldefense.com/v3/__https://www.windy.com/31.610/130.822?31.079,130.823,8,m:eDcaj1j__;!!IIc8w16ASern!s75gXtjarzrJN36-6kKlEwArMHnZiVXgMQkn-PlVnoGXmd0fc9Ra18tOYBJtXtKpG3JhjvCtc_11By85jwAEfa2rg0kPPK52KQ$"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        },
+         {
+            name: "肝付",
+
+            sublinks: [
+                {
+                    name: "windy",
+                    url: "https://urldefense.com/v3/__https://www.windy.com/31.313/131.035?30.782,131.037,8,m:eDcaj1j__;!!IIc8w16ASern!ooUp5FciArcqP48BXnGTK2AY1xwswiQodqhImADBY5yPtMa46AzDhTtFyfbYlWptuuuoFnkObvjBGSZq74Dzf8Vgu2IGhk7HzA$"
+                },
+                {
+                    name: "Yhoo!",
+                    url: ""
+                },
+                {
+                    name: "天気.com",
+                    url: ""
+                },
+            ]
+        }
+    ]
+}
+
+ 
+
+    
+];
+
+
+// 地図を取得
+const map = document.getElementById("map");
+
+
+// 地点を1つずつ作る
+locations.forEach(location => {
+
+    // 地点全体
+    const point = document.createElement("div");
+
+    point.className = "point";
+
+
+    // 地図上の位置
+    point.style.left = location.left + "%";
+    point.style.top = location.top + "%";
+
+
+    // リンクメニューを作る
+ const linkMenu = location.links.map(link => {
+
+    // さらに下のメニューがある場合
+    if (link.sublinks) {
+
+        const subMenu = link.sublinks.map(subLink => {
+
+            return `
+                <a href="${subLink.url}" target="_blank" rel="noopener noreferrer">
+                    ${subLink.name}
+                </a>
+            `;
+
+        }).join("");
+
+
+        return `
+            <div class="menu-item has-submenu">
+
+                <div class="menu-link">
+                    ${link.name}
+                    <span class="arrow">▶</span>
+                </div>
+
+                <div class="sub-menu">
+                    ${subMenu}
+                </div>
+
+            </div>
+        `;
+
+    }
+
+
+    // 普通のリンク
+    return `
+        <a href="${link.url}" target="_blank" rel="noopener noreferrer">
+            ${link.name}
+        </a>
+    `;
+
+}).join("");
+
+
+    // HTMLを作る
+    point.innerHTML = `
+        <div class="point-marker">
+
+            <!-- 丸印 -->
+            <a href="#" aria-label="${location.name}"></a>
+
+            <!-- 地点名 -->
+            <span>${location.name}</span>
+
+            <!-- 複数リンク -->
+            <div class="link-menu">
+                ${linkMenu}
+            </div>
+
+        </div>
+    `;
+
+
+    // 地図に追加
+    map.appendChild(point);
+document.querySelectorAll(".menu-item.has-submenu").forEach(item => {
+
+    const menuLink = item.querySelector(".menu-link");
+
+    menuLink.addEventListener("click", function(event) {
+
+        // サブメニューを開く
+        event.preventDefault();
+
+        item.classList.toggle("open");
+
+    });
+
+});
+});
